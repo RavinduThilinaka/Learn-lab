8753509a304907a6de87d1a8c5396936bb6e273f
RavinduThilinaka <ruwaninanayakkara920@gmail.com>
2025-04-26 22:45:37 +0530

    update feedback controller and feedback entity

--- backend/Learn-Lab/src/main/java/com/learnlab/Learn/Lab/entity/FeedBack.java
+++ backend/Learn-Lab/src/main/java/com/learnlab/Learn/Lab/entity/FeedBack.java
@@ -10,5 +10,51 @@ import lombok.Data;
 @Entity
 public class FeedBack {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String rating;
+    private String comments;
 
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
 }
--- backend/Learn-Lab/src/main/java/com/learnlab/Learn/Lab/exception/FeedbackNotFoundException.java
+++ backend/Learn-Lab/src/main/java/com/learnlab/Learn/Lab/exception/FeedbackNotFoundException.java
@@ -1,5 +1,7 @@
 package com.learnlab.Learn.Lab.exception;
 
 public class FeedbackNotFoundException extends RuntimeException{
-
+    public FeedbackNotFoundException(Long id){
+        super("Not Found exception id: "+id);
+    }
 }
