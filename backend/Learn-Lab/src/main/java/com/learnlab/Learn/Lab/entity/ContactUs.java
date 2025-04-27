54e97bb10e15b1b40a5fbe7134d511f4ac6f6674
RavinduThilinaka <ravindu2001thilinaka@gmail.com>
2025-04-27 13:00:38 +0530

    update contact backend

--- backend/Learn-Lab/src/main/java/com/learnlab/Learn/Lab/entity/ContactUs.java
+++ backend/Learn-Lab/src/main/java/com/learnlab/Learn/Lab/entity/ContactUs.java
@@ -1,4 +1,69 @@
 package com.learnlab.Learn.Lab.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class ContactUs {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private Integer mobile;
+    private String subject;
+    private String message;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(Integer mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- backend/Learn-Lab/src/main/java/com/learnlab/Learn/Lab/repository/ContactRepo.java
+++ backend/Learn-Lab/src/main/java/com/learnlab/Learn/Lab/repository/ContactRepo.java
@@ -1,4 +1,7 @@
 package com.learnlab.Learn.Lab.repository;
 
-public interface ContactRepo {
+import com.learnlab.Learn.Lab.entity.ContactUs;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactRepo extends JpaRepository<ContactUs,Long> {
 }
